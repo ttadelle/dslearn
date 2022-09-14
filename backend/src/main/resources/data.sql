165671260de2025b08fed676825ed98b319660f4
Thiago Tadelle <thiago.tadelle@gmail.com>
2022-09-14 15:09:11 -0300

    Enrollment seed

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -15,8 +15,8 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'https://937216.smushcdn.com/2561330/wp-content/uploads/2021/11/online759.jpg?lossy=1&strip=1&webp=1', 'https://img.myloview.com.br/posters/distance-learning-black-line-icon-online-training-courses-home-leisure-isolated-on-white-background-vector-elements-for-graphic-design-website-or-banner-700-208517440.jpg');
 
-INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0',TIMESTAMP WITH TIME ZONE '2022-07-13T03:00:00Z', TIMESTAMP WITH TIME ZONE '2023-07-13T03:00:00', 1);
-INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0',TIMESTAMP WITH TIME ZONE '2022-07-14T03:00:00Z', TIMESTAMP WITH TIME ZONE '2023-07-14T03:00:00', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0',TIMESTAMP WITH TIME ZONE '2022-07-13T03:00:00Z', TIMESTAMP WITH TIME ZONE '2023-07-13T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0',TIMESTAMP WITH TIME ZONE '2022-07-14T03:00:00Z', TIMESTAMP WITH TIME ZONE '2023-07-14T03:00:00Z', 1);
 
 INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://937216.smushcdn.com/2561330/wp-content/uploads/2021/11/online759.jpg?lossy=1&strip=1&webp=1', 1, 1);
 INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) VALUES ('Forum', 'Tire suas dúvidas', 2, 'https://937216.smushcdn.com/2561330/wp-content/uploads/2021/11/online759.jpg?lossy=1&strip=1&webp=1', 2, 1);
@@ -26,4 +26,5 @@ INSERT INTO tb_section (title, description, position, img_uri, resource_id, pre_
 INSERT INTO tb_section (title, description, position, img_uri, resource_id, pre_requisite_id) VALUES ('Capítulo 2', 'Neste capítulo iremos continuar', 2, 'https://937216.smushcdn.com/2561330/wp-content/uploads/2021/11/online759.jpg?lossy=1&strip=1&webp=1', 1, 1);
 INSERT INTO tb_section (title, description, position, img_uri, resource_id, pre_requisite_id) VALUES ('Capítulo 3', 'Neste capítulo iremos finalizar', 3, 'https://937216.smushcdn.com/2561330/wp-content/uploads/2021/11/online759.jpg?lossy=1&strip=1&webp=1', 1, 2);
 
---(AVALIABLE, ENROLL_MOMENT, ONLY_UPDATE, REFUND_MOMENT, OFFER_ID, USER_ID) 
+INSERT INTO tb_enrollment (USER_ID, OFFER_ID, ENROLL_MOMENT, REFUND_MOMENT, AVALIABLE, ONLY_UPDATE) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2023-07-13T13:00:00Z', null, true, false);
+INSERT INTO tb_enrollment (USER_ID, OFFER_ID, ENROLL_MOMENT, REFUND_MOMENT, AVALIABLE, ONLY_UPDATE) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2023-07-14T13:00:00Z', null, true, false);
